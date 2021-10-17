e1118fc06b5d76c26e0dcf5143b5d97148d47762
Agboola Iyanu <agboolasolomoniyanu@gmail.com>
2021-10-17 15:32:57 +0100

    added side image

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,10 +5,15 @@ import './Layout.scss';
 const Layout = ({ children, step }) => {
   return (
     <div className='layout'>
-      <div className='layout1'>
-        <Header step={step} />
+      <div className='layout-left'>
+        <img src='./image.png' alt='imag' />
       </div>
-      <div className='layout2'>{children}</div>
+      <div className='layout-right'>
+        <div className='layout1'>
+          <Header step={step} />
+        </div>
+        <div className='layout2'>{children}</div>
+      </div>
     </div>
   );
 };
